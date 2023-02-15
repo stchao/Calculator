cb4e324fddb39d12d046d5b7e50d14aaeaeb5d42
Steve Chao <22403555+stchao@users.noreply.github.com>
2023-02-15 10:52:15 -0500

    Add keyboard listener and operator disable toggle
    - Fix issue where user could enter leading zeros without a decimal
    - Add a leading zero before decimal if no number before it
    - Default display is the number 0 on load and on clear
    - Add ability for user to toggle operations in case of initial misclick

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', onLoad);
 
-function onLoad() {
-    setButtons();
-}
-
-const currentCalculation = {
+const storedValues = {
     num1: '',
     num2: '',
     operator: '',
-    previousButton: '',
+    operatorButtonName: '',
+}
+
+function onLoad() {
+    setButtons();
 }
 
 function setButtons() {
@@ -17,26 +17,53 @@ function setButtons() {
     let clearButton = container.querySelector("#clearButton");
     let githubButton = container.querySelector("#githubButton");
 
+    window.addEventListener("keydown", onKeyPress);
     buttons.map((button) => button.addEventListener("click", onButtonClick));
     githubButton.addEventListener("click", navigateToGithub);
     clearButton.addEventListener("click", clearCalculation)
 }
 
+function onKeyPress(ev) {
+    switch (ev.key) {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+        case '+':
+        case '-':
+        case '=':
+        case '*':
+        case '/':
+        case 'Enter':
+            ev.preventDefault();
+            onButtonClick(ev);
+            break;
+        default:
+    }
+}
+
 function onButtonClick(ev) {
-    let buttonValue = ev.target.value;
+    let buttonValue = ev.key || ev.target.value;
     let isNumber = !isNaN(Number(buttonValue));
     let isOperateSuccessful = true;
-    let isOperatorPickedAlready = currentCalculation.operator;
-    let isOperatorMisclicked = buttonValue === currentCalculation.previousButton;
+    let isOperatorPickedAlready = storedValues.operator;
     let isEqualClicked = buttonValue === '=';
 
     if (isNumber && !isOperatorPickedAlready) {
         updateNum(buttonValue, "num1");
+        toggleOperatorButton(); // toggle existing disabled, if any 
         return;
     }
 
     if (isNumber && isOperatorPickedAlready) {
         updateNum(buttonValue, "num2");
+        toggleOperatorButton(); // toggle existing disabled, if any 
         return;
     }
 
@@ -47,15 +74,16 @@ function onButtonClick(ev) {
         return;
     }    
 
-    if (isOperatorPickedAlready && !isOperatorMisclicked) {
+    if (isOperatorPickedAlready && storedValues.num2) {
         isOperateSuccessful = operate();
     }
 
     if (isOperateSuccessful && !isEqualClicked) {
-        currentCalculation.operator = buttonValue;
+        toggleOperatorButton(); // toggle existing disabled, if any  
+        storedValues.operator = buttonValue;        
+        storedValues.operatorButtonName = getButtonNameByValue(buttonValue);
+        toggleOperatorButton(); // toggle new operator
     }
-
-    currentCalculation.previousButton = buttonValue;
 }
 
 function add(num1, num2) {
@@ -75,11 +103,12 @@ function divide(num1, num2) {
 }
 
 function operate() {
-    let num1 = Number(currentCalculation.num1);
-    let num2 = Number(currentCalculation.num2);    
+    let num1 = Number(storedValues.num1);
+    let num2 = Number(storedValues.num2);    
     let result = "Invalid op";
+    
 
-    switch (currentCalculation.operator) {
+    switch (storedValues.operator) {
         case "+":
             result = add(num1, num2);
             break;
@@ -100,7 +129,8 @@ function operate() {
             break;
     }
     
-    let isValidNumber = !isNaN(result);
+    let isValidNumber = !isNaN(result);    
+    let buttonName = getButtonNameByValue(storedValues.operator);
 
     clearCalculation();
 
@@ -108,32 +138,34 @@ function operate() {
         updateDisplay(result);
         return false;
     }
-
+    
+    storedValues.operatorButtonName = buttonName;
+    toggleOperatorButton();
     updateNum(result, "num1");
     return true;
 }
 
 function otherOperate(symbol) {    
-    let isOperatorPickedAlready = currentCalculation.operator;
+    let isOperatorPickedAlready = storedValues.operator;
     let numProperty = !isOperatorPickedAlready ? "num1" : "num2";
     let isValidCondition = false;
-    let newNum = currentCalculation[numProperty];
-    currentCalculation.previousButton = symbol;
+    let newNum = storedValues[numProperty];
 
     switch (symbol) {
         case "+/-":
             // is current num negative
-            isValidCondition = currentCalculation[numProperty].includes("-");
-            newNum = `-${currentCalculation[numProperty]}`;
+            isValidCondition = storedValues[numProperty].includes("-");
+            newNum = `-${storedValues[numProperty]}`;
 
             if (isValidCondition) {
-                newNum = currentCalculation[numProperty].replace('-', '');
+                newNum = storedValues[numProperty].replace('-', '');
             }
             break;
         case ".":
             // is num already a decimal
-            isValidCondition = currentCalculation[numProperty].includes(".");
-            newNum = `${currentCalculation[numProperty]}.`;
+            isValidCondition = storedValues[numProperty].includes(".");
+            // adds 0 in front, if no num before decimal
+            newNum = `${(storedValues[numProperty] || '0')}.`;
 
             if (isValidCondition) {
                 return;
@@ -143,14 +175,43 @@ function otherOperate(symbol) {
             return;
     }
 
-    currentCalculation[numProperty] = '';            
+    storedValues[numProperty] = '';            
     updateNum(newNum, numProperty);   
 }
 
 function updateNum(num, numProperty = "num1") {
-    currentCalculation[numProperty] += num;
-    currentCalculation.previousButton = num;
-    updateDisplay(currentCalculation[numProperty]);
+    let isDecimal = String(num).includes(".") || storedValues[numProperty].includes(".");
+    let tempNum = storedValues[numProperty] + num;
+    storedValues[numProperty] = isDecimal ? tempNum : String(Number(tempNum));
+    updateDisplay(storedValues[numProperty]);
+}
+
+function toggleOperatorButton() {
+    if (!storedValues.operatorButtonName || !storedValues.operator) {
+        return;
+    }
+
+    let operatorButton = document.getElementById(storedValues.operatorButtonName);
+    operatorButton.disabled = !operatorButton.disabled;
+
+    if (!operatorButton.disabled) {
+        storedValues.operatorButtonName = '';
+    }
+}
+
+function getButtonNameByValue(value) {
+    switch (value) {
+        case "+":
+            return "addButton";
+        case "-":
+            return "subtractButton";
+        case "/":
+            return "divideButton";
+        case "*":
+            return "multiplyButton";
+        default:
+            return "";
+    }
 }
 
 function updateDisplay(num = '') {
@@ -159,10 +220,11 @@ function updateDisplay(num = '') {
 }
 
 function clearCalculation() {
-    currentCalculation.num1 = '';
-    currentCalculation.num2 = '';
-    currentCalculation.operator = '';
-    updateDisplay();
+    toggleOperatorButton();    
+    for (let calcProp in storedValues) {
+        storedValues[calcProp] = '';
+    }
+    updateDisplay("0");
 }
 
 function navigateToGithub() {
